d14f61236a4629bf8e8b5268d3e9d9459293edab
gcoronar-prog <gcoronarc4621@gmail.com>
2026-09-19 23:56:46 -0300

    minicorreccion

--- tienda/src/main/java/com/tienda_online/tienda/exception/GlobalExceptionHandler.java
+++ tienda/src/main/java/com/tienda_online/tienda/exception/GlobalExceptionHandler.java
@@ -7,8 +7,10 @@ import java.util.Map;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+@ControllerAdvice 
 public class GlobalExceptionHandler {
      @ExceptionHandler(RuntimeException.class)
     public ResponseEntity<Object> handleRuntimeException(RuntimeException ex) {
